a6eec908adc4935a9f9a1adeb28edc70cd657f48
KaterynaPryimakova <134732205+KaterynaPryimakova@users.noreply.github.com>
2024-03-03 22:49:53 +0200

    connect db

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import mongoose from "mongoose";
 
 import contactsRouter from "./routes/contactsRouter.js";
 
@@ -13,14 +14,24 @@ app.use(express.json());
 app.use("/api/contacts", contactsRouter);
 
 app.use((_, res) => {
-  res.status(404).json({ message: "Route not found" });
+    res.status(404).json({ message: "Route not found" });
 });
 
 app.use((err, req, res, next) => {
-  const { status = 500, message = "Server error" } = err;
-  res.status(status).json({ message });
+    const { status = 500, message = "Server error" } = err;
+    res.status(status).json({ message });
 });
 
 app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
+    console.log("Server is running. Use our API on port: 3000");
 });
+
+const DB_HOST =
+    "mongodb+srv://Kat:jnUnn8Aq65IwxwEK@cluster0.s9iqugu.mongodb.net/db-contacts?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose
+    .connect(DB_HOST)
+    .then(() => console.log("Database connection successful"))
+    .catch((error) =>
+        console.log(`Server not running. Error message: ${error.message}`)
+    );
